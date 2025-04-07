5188606e1a56fb4fbebcc86a712a9aec40c00e6d
Sébastien Moretti <smoretti@unil.ch>
2025-04-07 14:34:34 +0200

    Show Latin species name in the title

--- src/components/Gene/GeneDetails.js
+++ src/components/Gene/GeneDetails.js
@@ -84,7 +84,7 @@ const GeneDetails = ({
         .replace(':speciesId', geneMappedToSameGeneIdCount === 1 ? '' : species.id)
         .replace(/\/$/, '')}`;
     return {
-      title: `${name} - ${geneId} - expression in ${speciesName}`,
+      title: `${name} - ${geneId} expression in ${latinName}${speciesNameBrackets}`,
       description: `Bgee gene expression data for ${hasNameOpener}${geneId}${hasNameCloser} in ${latinName}${speciesNameBrackets}`,
       keywords: `gene expression, ${nameExpr}${geneId}, ${geneId} expression${synonymsExpr}`,
       link: canonicalLink,
